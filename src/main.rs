8aa5e5b7bc4dc921c9df24f595f80953e7dec8ff
Hrólfur E <106666932+hrolfurinn@users.noreply.github.com>
2024-05-20 02:03:10 -0400

    Trying tokio client connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use mini_redis::{client,Result};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("Got value from server; result = {:?}", result);
+
+    Ok(())
 }
